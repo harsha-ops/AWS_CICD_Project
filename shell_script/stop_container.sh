722a1f5a80575c9450afcef61988721cef83c010
harsha <harsha.venkata220@gmail.com>
2024-12-18 22:31:00 +0000

    fix in shell script file

--- shell_script/stop_container.sh
+++ shell_script/stop_container.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Stop the running container (if any)
-containerid=$(docker ps -aq)
-docker rm $containerid
+containerid=`docker ps -a | awk -F " " '{print $1}'`
+docker rm -f $containerid
